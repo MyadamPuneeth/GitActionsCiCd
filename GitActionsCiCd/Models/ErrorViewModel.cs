bf8fcdacf627a8c68b585b138b9d25e1e8cff5fc
Puneeth Myadam <myadampuneeth@gmail.com>
2025-02-01 12:25:15 +0530

    Add project files.

---
+++ GitActionsCiCd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitActionsCiCd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
